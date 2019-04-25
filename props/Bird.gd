e06a22b0e282c972c7c806991ef235a62aa03c5b
Leandro Voltolino <xupisco@gmail.com>
2019-04-25 20:34:50 -0300

    bird can fly

--- props/Bird.gd
+++ props/Bird.gd
@@ -1,18 +1,37 @@
 extends RigidBody2D
 
 export(bool) var flying = false
+export(Vector2) var flap_force = Vector2()
 var game_started:bool = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	if not flying:
-		$spr.playing = false;
-		mode = MODE_KINEMATIC
+    if not flying:
+        $spr.playing = false;
+        mode = MODE_KINEMATIC
 
 
 func _process(delta):
-	if flying and not game_started:
-		$spr.playing = true
-		mode = MODE_CHARACTER
-		game_started = true
+    if flying and not game_started:
+        mode = MODE_CHARACTER
+        fly()
+        game_started = true
 
+
+func fly():
+    $spr.frame = 1
+    $spr.playing = true
+    linear_velocity = Vector2.ZERO
+    apply_impulse(Vector2.UP, flap_force)
+
+
+func _input(event):
+    if event.is_action_pressed("flap"):
+        fly() 
+
+
+func _physics_process(delta):
+    pass
+
+func _on_spr_animation_finished():
+    $spr.playing = false;
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -4,20 +4,20 @@ extends Node2D
 var playing: bool = false;
 
 func _ready():
-	pass
+    pass
 
 
 func _process(delta):
-	pass
+    pass
 
 
 func begin_game():
-	$Bird.flying = true
-	$Ground.moving = true
-	$info.hide()
-	playing = true
+    $Bird.flying = true
+    $Ground.moving = true
+    $info.hide()
+    playing = true
 
 
 func _input(event):
-    if event is InputEventMouseButton and not playing:
+    if event.is_action_pressed("flap"):
         begin_game()
